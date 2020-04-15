cd1a6a9769463ecf71f74d1055620bf3adf52582
miguel-miranda <ayandram7727@gmail.com>
2020-04-15 18:54:33 -0300

    todos os casos de uso foram testados, acredito

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -35,3 +35,35 @@ test('botão de entrar é desabilitado se email é inválido', () => {
   const loginSubmitBtn = getByTestId('login-submit-btn');
   expect(loginSubmitBtn.disabled).toBeTruthy();
 });
+
+test('botão de entrar é desabilitado se senha é inválida', () => {
+  const { getByTestId } = render(<Login />);
+
+  const emailInput = getByTestId('email-input');
+  const validEmail = 'aaa@bbb.com';
+
+  const passwordInput = getByTestId('password-input');
+  const invalidPassWord = '123456';
+
+  fireEvent.change(emailInput, { target: { value: validEmail } });
+  fireEvent.change(passwordInput, { target: { value: invalidPassWord } });
+
+  const loginSubmitBtn = getByTestId('login-submit-btn');
+  expect(loginSubmitBtn.disabled).toBeTruthy();
+});
+
+test('botão de entrar é habilitado se email e senha são válidos', () => {
+  const { getByTestId } = render(<Login />);
+
+  const emailInput = getByTestId('email-input');
+  const validEmail = 'aaa@bbb.com';
+
+  const passwordInput = getByTestId('password-input');
+  const validPassWord = '1234567';
+
+  fireEvent.change(emailInput, { target: { value: validEmail } });
+  fireEvent.change(passwordInput, { target: { value: validPassWord } });
+
+  const loginSubmitBtn = getByTestId('login-submit-btn');
+  expect(loginSubmitBtn.disabled).toBeFalsy();
+});
