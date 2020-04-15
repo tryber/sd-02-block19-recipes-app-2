2788829d308d83ba811f72f9ca29524c8b8fcedc
miguel-miranda <ayandram7727@gmail.com>
2020-04-15 18:40:37 -0300

    tentando construir alguns testes para a pagina de Login

---
+++ src/tests/Login.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import Login from '../pages/Login';
+
+afterEach(cleanup);
+
+test('exibe todos os elementos da página', () => {
+  const { getByText, getByTestId } = render(<Login />);
+
+  const pageTitle = getByText(/Login/i);
+  expect(pageTitle).toBeInTheDocument();
+
+  const emailInput = getByTestId('email-input');
+  expect(emailInput).toBeInTheDocument();
+
+  const passwordInput = getByTestId('password-input');
+  expect(passwordInput).toBeInTheDocument();
+
+  const loginSubmitBtn = getByTestId('login-submit-btn');
+  expect(loginSubmitBtn).toBeInTheDocument();
+});
+
+test('botão de entrar é desabilitado se email é inválido', () => {
+  const { getByTestId } = render(<Login />);
+
+  const emailInput = getByTestId('email-input');
+  const invalidEmail = 'aaa@bbb';
+
+  const passwordInput = getByTestId('password-input');
+  const validPassWord = '1234567';
+
+  fireEvent.change(emailInput, { target: { value: invalidEmail } });
+  fireEvent.change(passwordInput, { target: { value: validPassWord } });
+
+  const loginSubmitBtn = getByTestId('login-submit-btn');
+  expect(loginSubmitBtn.disabled).toBeTruthy();
+});
