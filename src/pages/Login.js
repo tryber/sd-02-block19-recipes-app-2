ff55264200fd066a866f347b40578cb267b27a2c
miguel-miranda <ayandram7727@gmail.com>
2020-04-17 16:28:10 -0300

    Todos os requisitos especificos da tela de login foram cumpridos

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
 const Login = () => {
   const [emailIsValid, setEmailIsValid] = useState(false);
   const [passwordIsValid, setPasswordIsValid] = useState(false);
+  const [email, setEmail] = useState('');
+  const [shouldRedirect, setShouldRedirect] = useState(false);
 
   const validateEmail = ({ target }) => {
     const { value } = target;
     const regex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+    setEmail(value);
     return regex.test(value) ? setEmailIsValid(true) : setEmailIsValid(false);
   };
 
@@ -15,7 +19,17 @@ const Login = () => {
     return (value.length > 6) ? setPasswordIsValid(true) : setPasswordIsValid(false);
   };
 
-  return (
+  const submitForm = () => {
+    localStorage.setItem('meals-token', '1');
+    localStorage.setItem('cocktails-token', '1');
+
+    const emailObject = { email };
+    localStorage.setItem('user', JSON.stringify(emailObject));
+
+    setShouldRedirect(true);
+  };
+
+  return shouldRedirect ? <Redirect to="/comidas" /> : (
     <div>
       <h1>Login</h1>
       <input placeholder="Email" data-testid="email-input" onChange={validateEmail} />
@@ -24,6 +38,7 @@ const Login = () => {
         type="button"
         data-testid="login-submit-btn"
         disabled={!(emailIsValid && passwordIsValid)}
+        onClick={submitForm}
       >
         Entrar
       </button>
