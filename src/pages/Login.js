51606b7f205809343dfeab01a5eafc9ce83b1178
miguel-miranda <ayandram7727@gmail.com>
2020-04-18 02:31:04 -0300

    100% de cobertura de testes em src/pages/Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { RecipesAppContext } from '../context/RecipesAppContext';
 
 const validateEmail = ({ target }, setEmail, setEmailIsValid) => {
@@ -14,14 +14,12 @@ const validatePassword = ({ target }, setPasswordIsValid) => {
   return (value.length > 6) ? setPasswordIsValid(true) : setPasswordIsValid(false);
 };
 
-const submitForm = (email, setShouldRedirect) => {
+const submitForm = (email) => {
   localStorage.setItem('meals-token', '1');
   localStorage.setItem('cocktails-token', '1');
 
   const emailObject = { email };
   localStorage.setItem('user', JSON.stringify(emailObject));
-
-  setShouldRedirect(true);
 };
 
 const renderLoginPage = (setEmail, setEmailIsValid, setPasswordIsValid,
@@ -39,14 +37,16 @@ const renderLoginPage = (setEmail, setEmailIsValid, setPasswordIsValid,
       data-testid="password-input"
       onChange={(event) => validatePassword(event, setPasswordIsValid)}
     />
-    <button
-      type="button"
-      data-testid="login-submit-btn"
-      disabled={!(emailIsValid && passwordIsValid)}
-      onClick={() => submitForm(email, setShouldRedirect)}
-    >
-      Entrar
-    </button>
+    <Link to="/comidas">
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={!(emailIsValid && passwordIsValid)}
+        onClick={() => submitForm(email)}
+        >
+        Entrar
+      </button>
+    </Link>
   </div>
 );
 
@@ -58,21 +58,12 @@ const Login = () => {
   const [emailIsValid, setEmailIsValid] = useState(false);
   const [passwordIsValid, setPasswordIsValid] = useState(false);
   const [email, setEmail] = useState('');
-  const [shouldRedirect, setShouldRedirect] = useState(false);
 
-  useEffect(
-    () => {
-      setDisplayHeader(false);
-      setDisplayFooter(false);
-    },
-    [],
-  );
+  setDisplayHeader(false);
+  setDisplayFooter(false);
 
-  return (
-    shouldRedirect ?
-      <Redirect to="/comidas" /> :
-      renderLoginPage(setEmail, setEmailIsValid, setPasswordIsValid,
-        emailIsValid, passwordIsValid, email, setShouldRedirect)
+  return renderLoginPage(setEmail, setEmailIsValid, setPasswordIsValid,
+      emailIsValid, passwordIsValid, email,
   );
 };
 
