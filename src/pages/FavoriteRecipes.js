50575694eedcfc0cdd192038529bc87864d0a9e0
miguel-miranda <ayandram7727@gmail.com>
2020-04-24 11:12:30 -0300

    Correçoes CC no CSS

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,16 +1,16 @@
 import React from 'react';
-// import HorizontalCard from '../components/HorizontalCard'
-// import imagemTeste from '../images/imagemTeste.svg';
+import HorizontalCard from '../components/HorizontalCard'
+import imagemTeste from '../images/imagemTeste.svg';
 
 const FavoriteRecipes = () => (
   <div>
     <h1>Favorite Recipes Page</h1>
-    {/* <HorizontalCard
+    <HorizontalCard
       index={1} type="comida" id="453"
       image={imagemTeste}
       name="Chelsea Buns" category="Dessert" area="British" alcoholicOrNot="Alcoholic Drink"
       doneDate="01/12/2019" tags={['bun', 'baking']}
-    /> */}
+    />
   </div>
 );
 
