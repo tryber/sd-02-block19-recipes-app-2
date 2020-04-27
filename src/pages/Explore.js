046fd90a864b0823bcc35e298e25f6b417ed041e
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-04-27 12:22:07 -0300

    Explore page and test 100%

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-const Explore = () => (
-  <div>Explore Page</div>
+import { RecipesAppContext } from '../context/RecipesAppContext';
+
+const renderExploreButtons = (type, testid) => (
+  <Link to={`/explorar/${type.toLowerCase()}`}>
+    <button
+      type="button"
+      data-testid={`explore-${testid}`}
+    >
+      {`Explorar ${type}`}
+    </button>
+  </Link>
 );
+
+const Explore = () => {
+  const {
+    displayHeader: [, setDisplayHeader], displayFooter: [, setDisplayFooter],
+  } = useContext(RecipesAppContext);
+
+  useEffect(() => {
+    setDisplayHeader(true);
+    setDisplayFooter(true);
+  }, [setDisplayFooter, setDisplayHeader]);
+
+  return (
+    <div>
+      {renderExploreButtons('Comidas', 'food')}
+      {renderExploreButtons('Bebidas', 'drinks')}
+    </div>
+  );
+};
+
 
 export default Explore;
