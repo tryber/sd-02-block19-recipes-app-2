05d6d5efcd8403a3bbaff98b6c29faf09dbe3d10
miguel-miranda <ayandram7727@gmail.com>
2020-04-27 10:11:27 -0300

    CC

--- src/components/HorizontalCard.js
+++ src/components/HorizontalCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import ShareButton from './ShareButton';
 import FavoriteButton from './FavoriteButton';
 import '../styles/HorizontalCard.css';
-import { Link } from 'react-router-dom';
 
 const HorizontalCard = ({ index, type, id, image, name, category, area, alcoholicOrNot,
   doneDate, tags }) => {
