e4937c652d54a25a2898b1d4fb8628b23802a7e9
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-04-28 15:30:52 -0300

    CC

--- src/components/RenderCarousel.js
+++ src/components/RenderCarousel.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
 import { fetchMealByAllCategories } from '../services/mealPageApis';
 import { fetchDrinkByAllCategories } from '../services/drinkPageApis';
-import { Link } from 'react-router-dom';
 
 const responsive = {
   superLargeDesktop: {
