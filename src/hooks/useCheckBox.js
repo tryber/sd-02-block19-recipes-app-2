6b29a4eadf33b395ad93c1e68c707372356443e9
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-04-27 21:40:07 -0300

    CC

--- src/hooks/useCheckBox.js
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from 'react';
-
-const useCheckBox = () => {
-  const [checkbox, setCheckbox] = useState([]);
-
-  return [checkbox, setCheckbox];
-};
-
-export default useCheckBox;
