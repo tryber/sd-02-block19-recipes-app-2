f22919d9e6e3cd03de22736034045e86513124b2
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-04-27 23:31:55 -0300

    Coverage total 93%

---
+++ src/__mocks__/ingredientsMock.js
@@ -0,0 +1,57 @@
+export const mealIngredients = {
+  meals: [
+    {
+      strIngredient: 'Chicken',
+    },
+    {
+      strIngredient: 'Salmon',
+    },
+    {
+      strIngredient: 'Beef',
+    },
+    {
+      strIngredient: 'Pork',
+    },
+    {
+      strIngredient: 'Avocado',
+    },
+    {
+      strIngredient: 'Apple Cider Vinegar',
+    },
+    {
+      strIngredient: 'Bacon',
+    },
+    {
+      strIngredient: 'Basil',
+    },
+    {
+      strIngredient: 'Cheese',
+    },
+    {
+      strIngredient: 'Carrot',
+    },
+  ],
+};
+
+export const drinkIngredients = {
+  drinks: [
+    {
+      strIngredient1: 'Lemon',
+    },
+    {
+      strIngredient1: 'Applejack',
+    },
+    {
+      strIngredient1: 'Gin',
+    },
+    {
+      strIngredient1: 'Tea',
+    },
+    {
+      strIngredient1: 'Vodka',
+    },
+    {
+      strIngredient1: 'Grenadine',
+    },
+  ],
+};
