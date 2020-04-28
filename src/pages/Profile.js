32b0adb695aaf98f22f3658a3ae98855f9a98534
miguel-miranda <ayandram7727@gmail.com>
2020-04-28 16:28:34 -0300

    Pagina de perfil pronta, com CSS, sem testes

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RecipesAppContext } from '../context/RecipesAppContext';
+import '../styles/Profile.css';
 
-const Profile = () => (
-  <div>
-    <h1>Profile Page</h1>
-    <div />
+const setPageElements = (
+  setHeaderTitle,
+  setDisplayHeader,
+  setDisplaySearchButton,
+  setDisplayFooter,
+) => {
+  setHeaderTitle('Perfil');
+  setDisplayHeader(true);
+  setDisplaySearchButton(false);
+  setDisplayFooter(true);
+};
+
+const logout = () => {
+  localStorage.clear();
+};
+
+const renderProfileButtons = () => (
+  <div className="profile-buttons">
+    <a to="/receitas-feitas">
+      <button data-testid="profile-done-btn">
+        Receitas Feitas
+      </button>
+    </a>
+    <a to="/receitas-favoritas">
+      <button data-testid="profile-favorite-btn">
+        Receitas Favoritas
+      </button>
+    </a>
+    <a to="/">
+      <button onClick={logout} data-testid="profile-logout-btn">
+        Sair
+      </button>
+    </a>
   </div>
 );
+
+const Profile = () => {
+  const {
+    headerTitle: [, setHeaderTitle],
+    displayHeader: [, setDisplayHeader],
+    displaySearchButton: [, setDisplaySearchButton],
+    displayFooter: [, setDisplayFooter],
+  } = useContext(RecipesAppContext);
+
+  setPageElements(setHeaderTitle, setDisplayHeader, setDisplaySearchButton, setDisplayFooter);
+
+  const email = JSON.parse(localStorage.getItem('user')).email;
+
+  return (
+    <div className="profile">
+      <h1 data-testid="profile-email">{email}</h1>
+      {renderProfileButtons()}
+    </div>
+  );
+};
 
 export default Profile;
