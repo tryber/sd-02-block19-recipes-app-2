adefe42fd179a14f08e909e44e5b375948e3c921
Mateus Talles <mateustalles@gmail.com>
2020-05-11 10:21:47 -0300

    'All' categories button stays always on

--- src/pages/DrinkPage.js
+++ src/pages/DrinkPage.js
@@ -49,7 +49,7 @@ const renderCategories = (
       className="category-button"
       type="button"
       data-testid="all-category-filter"
-      disabled={(toggleCategory[0].category === 'All') ? disabled : toggleCategory[0].toggleCat}
+      // disabled={(toggleCategory[0].category === 'All') ? disabled : toggleCategory[0].toggleCat}
       onClick={() => {
         setToggleAndRecipes(toggleCategory, setRecipes, 'All', setIsFetching);
         setIsFiltering((prevState) => (!prevState));
