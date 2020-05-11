e9fe1194d5fab1fc08443d557c6ee577555b6ef1
Mateus Talles <mateustalles@gmail.com>
2020-05-11 10:53:14 -0300

    removed trailling coma and added dependencie on effects of RecipeFoodDetails

--- src/components/RecipeFoodsDetails.js
+++ src/components/RecipeFoodsDetails.js
@@ -30,13 +30,13 @@ const RecipeFoodDetails = ({ id, typeFood }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [carousel, setCarousel] = useState({ isLoading: false, data: [] });
   const detailsRecipe = useFoodById(id, typeFood) || [];
-  const { setDisplay, displaySearchBar: [, setDisplaySearchBar], } = useContext(RecipesAppContext);
+  const { setDisplay, displaySearchBar: [, setDisplaySearchBar] } = useContext(RecipesAppContext);
 
   useEffect(() => {
     setDisplay(false, false, false);
     if (detailsRecipe.length > 0) setIsLoading(false);
     setDisplaySearchBar(false);
-  }, [setDisplay, detailsRecipe]);
+  }, [setDisplay, detailsRecipe, setDisplaySearchBar]);
 
   if (isLoading) return <div>Loading...</div>;
   return (
